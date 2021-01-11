5bb5eb3d8efdd25fae18288574b69ea1ec154457
Jean-Luc Parouty <Jean-Luc.Parouty@grenoble-inp.fr>
2021-01-11 01:16:04 +0100

    Add error handling to ci test process

--- fidle/cookci.py
+++ fidle/cookci.py
@@ -158,19 +158,29 @@ def run_profile(profile, top_dir='..'):
         # ---- Top chrono
         #
         chrono_start()
-        update_ci_report(id, start=True)
+        update_ci_report(id, notebook_dir, notebook_src, notebook_out, start=True)
 
-        # ---- Run, run, run...
+        # ---- Try to run...
         #
         print('    Run notebook...',end='')
-        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
-        ep.preprocess(notebook)
-        print('..done.')
+        try:
+            ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+            ep.preprocess(notebook)
+        except Exception as e:
+            happy_end = False
+            notebook_out = notebook_src.replace('.ipynb', '==ERROR==.ipynb')
+            print('\n   ','*'*60)
+            print( '    ** AAARG.. An error occured : ',type(e).__name__)
+            print(f'    ** See notebook :  {notebook_out} for details.')
+            print('   ','*'*60)
+        else:
+            happy_end = True
+            print('..done.')
 
         # ---- Top chrono
         #
-        chrono_stop()
-        update_ci_report(id, end=True)
+        chrono_stop()        
+        update_ci_report(id, notebook_dir, notebook_src, notebook_out, end=True, happy_end=happy_end)
         print('    Duration : ',chrono_delay() )
     
         # ---- Save notebook
@@ -212,7 +222,7 @@ def reset_ci_report(verbose=True):
     if verbose : print(f'Finished file has been reset.\n')
     
     
-def update_ci_report(notebook_id, start=False, end=False):
+def update_ci_report(notebook_id, notebook_dir, notebook_src, notebook_out, start=False, end=False, happy_end=True):
     global start_time, end_time
 
     if not os.access(config.CI_REPORT_JSON, os.W_OK):
@@ -220,57 +230,51 @@ def update_ci_report(notebook_id, start=False, end=False):
     
     # ---- Load it
     with open(config.CI_REPORT_JSON) as fp:
-        data = json.load(fp)
+        report = json.load(fp)
         
     # ---- Update as a start
     if start is True:
-        data[notebook_id]             = {}
-        data[notebook_id]['path']     = os.getcwd()
-        data[notebook_id]['start']    = start_time.strftime("%A %-d %B %Y, %H:%M:%S")
-        data[notebook_id]['end']      = ''
-        data[notebook_id]['duration'] = 'Unfinished...'
+        report[notebook_id]             = {}
+        report[notebook_id]['dir']      = notebook_dir
+        report[notebook_id]['src']      = notebook_src
+        report[notebook_id]['out']      = notebook_out
+        report[notebook_id]['start']    = start_time.strftime("%d/%m/%y %H:%M:%S")
+        report[notebook_id]['end']      = ''
+        report[notebook_id]['duration'] = 'Unfinished...'
+        report[notebook_id]['state']    = 'Unfinished...'
 
     # ---- Update as an end
     if end is True:
-        data[notebook_id]['end']      = end_time.strftime("%A %-d %B %Y, %H:%M:%S")
-        data[notebook_id]['duration'] = chrono_delay()
+        report[notebook_id]['end']      = end_time.strftime("%d/%m/%y %H:%M:%S")
+        report[notebook_id]['duration'] = chrono_delay()
+        report[notebook_id]['state']    = 'ok' if happy_end else 'ERROR'
+        report[notebook_id]['out']      = notebook_out     # changeg in case of error
 
     # ---- Save it
     with open(config.CI_REPORT_JSON,'wt') as fp:
-        json.dump(data,fp,indent=4)
+        json.dump(report,fp,indent=4)
 
 
 
 def build_ci_report(display_output=True, save_html=True):
     
-    # ---- Load catalog (notebooks descriptions)
-    #
-    with open(config.CATALOG_FILE) as fp:
-        catalog = json.load(fp)
-
     # ---- Load ci report
     #
     with open(config.CI_REPORT_JSON) as infile:
         ci_report = json.load( infile )
 
-    # ---- For each entry
+    # ---- Create a nice DataFrame
     #
-    col_id, col_repo, col_name, col_start, col_end, col_dur = [],[],[],[],[],[]
-    for id,about in catalog.items():
-        if id in ci_report.keys():
-            ci_entry  = ci_report[id]
-            dirname   = about['dirname'] 
-            basename  = about['basename'] 
-            
-            col_id.append(   f'<a href="../{dirname}/{basename}">{id}</a>'               )
-            col_repo.append( dirname )
-            col_name.append( f'<a href="../{dirname}/{basename}"><b>{basename}</b></a>'  )
-            col_start.append( ci_entry['start'] )
-            col_end.append(   ci_entry['end']   )
-            col_dur.append(   ci_entry['duration']   )
-                   
-    df = pd.DataFrame( {'id':col_id, 'Repo':col_repo, 'Name':col_name, 'Start':col_start, 'End':col_end, 'Duration':col_dur} )
-
+    df=pd.DataFrame(ci_report)
+    df=df.transpose()
+    df = df.rename_axis('id').reset_index()
+    
+    # ---- Change text columns, for nice html links
+    #
+    df['id']  = df.apply(lambda r: f"<a href='../{r['dir']}/{r['src']}'>{r['id']}</a>", axis=1)
+    df['src'] = df.apply(lambda r: f"<a href='../{r['dir']}/{r['src']}'>{r['src']}</a>", axis=1)
+    df['out'] = df.apply(lambda r: f"<a href='../{r['dir']}/{r['out']}'>{r['out']}</a>", axis=1)
+        
     # ---- Add styles to be nice
     #
     styles = [
@@ -278,7 +282,7 @@ def build_ci_report(display_output=True, save_html=True):
         dict(selector="th", props=[("font-size", "110%"), ("text-align", "left")])
     ]
     def still_pending(v):
-        return 'background-color: OrangeRed; color:white' if v == 'Unfinished...' else ''
+        return 'background-color: OrangeRed; color:white' if v == 'ERROR' else ''
 
     output = df[df.columns.values].style.set_table_styles(styles).hide_index().applymap(still_pending)
 
